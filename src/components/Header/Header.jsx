88939242df2b2c44879c70d9d79cc6eb75186b23
Valeria Sihrianska <132007746+ValeriaSihr@users.noreply.github.com>
2025-05-30 12:47:34 +0100

    h

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,12 @@ import Container from "../Container/Container";
 import Navigation from "./Navigation/Navigation";
 
 export default function Header() {
-    return <header>
+    return (<header>
         <Container>
             <Navigation/>
             <Logo />
             <Cart />
             <Button/>                
         </Container>
-    </header>
+    </header>)
 }
